eee2827c539969ab94da16974b7b9cbecfb10471
tusharrr01 <tusharkaklotar81@gmail.com>
2025-04-19 13:11:16 +0530

    Create Routes And Models

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const startServer = async () => {
   const app = express();
   const port = 3000;
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+  // app.get('/', (req, res) => {
+  //   res.send('Hello World!');
+  // });
+  
+  // Available routes
+  app.use('/api/auth', require('./routes/auth'))
+  app.use('/api/notes', require('./routes/notes'))
 
   app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
